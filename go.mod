6df37c7aea76224a7922fc5813b06bc79db108bf
darkodi <darko.deuric@ethernal.tech>
2023-06-06 14:43:21 +0200

    puppy is dependent on dog package

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/darkodi/puppy
 
 go 1.20
+
+require github.com/darkodi/dog v0.0.0-20230606123908-072ab5cb862f // indirect
--- puppy.go
+++ puppy.go
@@ -1,9 +1,15 @@
 package puppy
 
+import "github.com/darkodi/dog"
+
 func Bark() string {
 	return "Woof!"
 }
 
 func Barks() string {
 	return "Woof! Woof! Woof!"
+}
+
+func BigBarks() string {
+	return dog.WhenGrownUp(Bark())
 }
